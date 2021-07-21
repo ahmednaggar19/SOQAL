c01d1830f24caca11659a061d78cbe45d5d86894
ahmednaggar19 <ahmed.naggar@incorta.com>
2021-07-21 15:15:14 +0200

    process multi outputs

--- huggingface/HuggingFaceModel.py
+++ huggingface/HuggingFaceModel.py
@@ -185,25 +185,27 @@ class HuggingFaceModel:
                                 return_overflowing_tokens=True,
                                 padding="max_length",
                                 truncation="only_second",
+                                return_offsets_mapping=True,
                                 add_special_tokens=True, return_tensors="pt")
         inputs.pop("overflow_to_sample_mapping")
         print("input_ids size", inputs["input_ids"].size())
-        input_ids = inputs["input_ids"].tolist()[0]
+        input_ids = inputs["input_ids"].tolist()
         outputs = self.model(**inputs)
+        answers = []
         print("outputs size", outputs.start_logits.size())
-        answer_start_scores = outputs.start_logits
+        for i, output in enumerate(outputs.start_logits):
+            answer_start_scores = output
+            answer_end_scores = outputs[i].end_logits
 
-        answer_end_scores = outputs.end_logits
+            answer_start_logit = torch.max(answer_start_scores)  # Get the most likely beginning of answer with the argmax of the score
+            answer_end_logit = torch.max(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
 
-        answer_start_logit = torch.max(answer_start_scores)  # Get the most likely beginning of answer with the argmax of the score
-        answer_end_logit = torch.max(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
+            answer_start = torch.argmax(answer_start_scores)  # Get the most likely beginning of answer with the argmax of the score
+            answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
 
-        answer_start = torch.argmax(answer_start_scores)  # Get the most likely beginning of answer with the argmax of the score
-        answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
-
-        answer = self.tokenizer.convert_tokens_to_string(self.tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-        return answer_start_logit, answer_end_logit, answer
-
+            answer = self.tokenizer.convert_tokens_to_string(self.tokenizer.convert_ids_to_tokens(input_ids[i][answer_start:answer_end]))
+            answers.append({answer_start_logit, answer_end_logit, answer}) 
+        return answers
     
     def predict_batch(self, examples):
         
@@ -224,17 +226,18 @@ class HuggingFaceModel:
             #         }
             #         idx += 1
             # else:
-            answer_start_logit, answer_end_logit, answer = self.query_model(example["question"], 
+            answers = self.query_model(example["question"], 
                         example["context"] 
                         # if len(example["context"]) < MAX_LENGTH else example["context"][:MAX_LENGTH]
                         )
-            nbest[str(idx)] = {}
-            nbest[str(idx)][0] = {
-                'start_logit': answer_start_logit,
-                'end_logit': answer_end_logit,
-                'text': answer
-            }
-            idx += 1
+            for answer in answers:
+                nbest[str(idx)] = {}
+                nbest[str(idx)][0] = {
+                    'start_logit': answer.answer_start_logit,
+                    'end_logit': answer.answer_end_logit,
+                    'text': answer.answer
+                }
+                idx += 1
 
 
         return nbest
