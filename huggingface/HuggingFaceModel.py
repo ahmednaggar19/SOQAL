cc59a34253cc13f072732e45abcf2b839d4d175c
ahmednaggar19 <ahmed.naggar@incorta.com>
2021-07-22 14:19:58 +0200

    fixed question instead of context problem

--- huggingface/HuggingFaceModel.py
+++ huggingface/HuggingFaceModel.py
@@ -234,7 +234,7 @@ class HuggingFaceModel:
             #         idx += 1
             # else:
             answers = self.query_model(arabert_prep.preprocess(example["question"]), 
-                        arabert_prep.preprocess(example["question"])
+                        arabert_prep.preprocess(example["context"])
                         # if len(example["context"]) < MAX_LENGTH else example["context"][:MAX_LENGTH]
                         )
             for answer in answers:
